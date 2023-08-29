37655459e488621bbf73100b9d10624f37f8d887
Rikerson Antonio Freitas Silva <1330864@sga.pucminas.br>
2023-08-29 13:28:20 -0300

    commit message

--- TP01E01.java
+++ TP01E01.java
@@ -1,4 +1,14 @@
 class TP01E1
   {
-    
+
+    public static boolean isPali()
+    {
+      boolean resp=false;
+
+      return resp;
+    }
+    public static void main (String[] args)
+    {
+
+    }
   }
