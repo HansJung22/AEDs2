d6ea6a8b42833370e9f3a19d36e8ec1d6346f087
Rikerson <rikersonfreitas88@gmail.com>
2023-08-31 11:41:11 -0700

    commit message

--- TP01/E01/Palindromo.java
+++ TP01/E01/Palindromo.java
@@ -1,14 +1,36 @@
-class Palindromo
+import java.io.*;
+
+class Palindromo 
+{
+
+  public static boolean IsPali(String input) 
   {
+    boolean resp = true;
 
-    public static boolean isPali()
+    for (int i = 0; i < input.length() / 2; i++) 
     {
-      boolean resp=false;
-
-      return resp;
-    }
-    public static void main (String[] args)
-    {
-
-    }
+      for (int j=input.length() - 1; j > input.length() / 2; j--) 
+      {
+        if (input.charAt(i) != input.charAt(j)) 
+	{
+          resp = false;
+          i = input.length();
+          j = 0;
+        } 
+      } 
+    } 
+    return resp;
   }
+  
+  public static void main(String[] paramArrayOfString) throws IOException 
+  {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    
+    String str = bufferedReader.readLine();
+    
+    System.out.println("Line: " + str);
+    
+    System.out.println("palindromo ?: " + IsPali(str));
+  }
+}
+
